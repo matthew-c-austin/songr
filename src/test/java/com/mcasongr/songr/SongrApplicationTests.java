0dd6a06fd275b9d9175160c021a8561d2cc97b6d
matthew-c-austin <maustin803@gmail.com>
2023-05-01 11:03:07 -0700

    initial commit from Spring init

---
+++ src/test/java/com/mcasongr/songr/SongrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcasongr.songr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
